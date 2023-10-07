dcf6346218a05d3408423c915c729095ae486f3f
Romuald Brillout <git@brillout.com>
2023-10-07 18:40:22 +0200

    fix serving of static assets

--- server/index_prod.js
+++ server/index_prod.js
@@ -16,7 +16,7 @@ startServer()
 async function startServer() {
   const app = express()
 
-  app.use(express.static(`${root}/dist/client`))
+  app.use(express.static(`${root}/client`))
 
   app.get('*', async (req, res, next) => {
     // It's important to create an entirely new instance of Apollo Client for each request.
